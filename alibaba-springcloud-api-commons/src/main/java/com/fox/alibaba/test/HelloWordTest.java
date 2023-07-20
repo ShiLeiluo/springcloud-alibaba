cff9da3c1d050ba5c7481f753e93d5447acb356f
ShiLeiluo <1549014363@qq.com>
2023-07-20 08:29:05 +0800

    design pattern

---
+++ alibaba-springcloud-api-commons/src/main/java/com/fox/alibaba/test/HelloWordTest.java
@@ -0,0 +1,15 @@
+package com.fox.alibaba.test;
+
+import com.fox.test.HelloWord;
+
+/**
+* @author F1337248 ShiLeiluo
+* @date 2023-07-18 14:54
+* @version 1.0
+*/
+public class HelloWordTest {
+	public static void main(String[] args) {
+		HelloWord helloWord = new HelloWord();
+		helloWord.sayHello();
+	}
+}
